47c9842612d89489b4030091bb280c1bff200b51
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-07-12 14:50:54 +0200

    created admin entity and tested it

---
+++ src/main/java/pl/sda/medicalcrm/entity/Admin.java
@@ -0,0 +1,45 @@
+package pl.sda.medicalcrm.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
+@DiscriminatorValue("Admin")
+public class Admin extends User {
+
+    private String name;
+    private String surname;
+
+    private Admin(){
+    }
+
+    public Admin(String login, String password, String name, String surname) {
+        super(login, password);
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Admin admin = (Admin) o;
+        return name.equals(admin.name) &&
+                surname.equals(admin.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, surname);
+    }
+}
