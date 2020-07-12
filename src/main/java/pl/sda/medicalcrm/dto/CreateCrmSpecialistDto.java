0687686e1284b6978cb4549f55da69565bebb5ce
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-07-12 14:07:04 +0200

    added validation to crm specialist dto

--- src/main/java/pl/sda/medicalcrm/dto/CreateCrmSpecialistDto.java
+++ src/main/java/pl/sda/medicalcrm/dto/CreateCrmSpecialistDto.java
@@ -1,12 +1,30 @@
 package pl.sda.medicalcrm.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.Objects;
 
 public class CreateCrmSpecialistDto {
 
+    @NotBlank
+    @NotEmpty
+    @Pattern(regexp = "[a-z]{6}")
     private String login;
+
+    @NotEmpty
+    //minimum 8 characters and at least one Capital letter, one lower letter, one digit and one special character
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$")
     private String password;
+
+    @NotEmpty
+    @NotBlank
+    @Pattern(regexp = "\\w[a-zA-Z]{1,}")
     private String name;
+
+    @NotEmpty
+    @NotBlank
+    @Pattern(regexp = "\\w[a-zA-Z\\-]{1,}")
     private String surname;
 
     public CreateCrmSpecialistDto(String login, String password, String name, String surname) {
