735e1f978cf4887b265ad5e9ef2a45a5ff9ac85c
Grzegorz Albrewczynski <g.albrewczynski@gmail.com>
2020-07-23 15:47:08 +0200

    AppointmentService test

--- src/main/java/pl/sda/medicalcrm/rest/AppointmentRestController.java
+++ src/main/java/pl/sda/medicalcrm/rest/AppointmentRestController.java
@@ -2,8 +2,15 @@ package pl.sda.medicalcrm.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.medicalcrm.entity.Appointment;
+import pl.sda.medicalcrm.entity.Doctor;
+import pl.sda.medicalcrm.entity.Specialization;
+import pl.sda.medicalcrm.entity.User;
 import pl.sda.medicalcrm.repository.AppointmentRepository;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/appointments")
@@ -11,4 +18,18 @@ public class AppointmentRestController {
 
     @Autowired
     private AppointmentRepository appointmentRepository;
+
+    @PostMapping(path = "/appointments")
+    public @ResponseBody Long createAppointment(@RequestBody @Valid Appointment appointment){
+        appointmentRepository.save(appointment);
+        return appointment.getId();
+    }
+
+    @PostMapping(path = "/{patientId}/appointment")
+    public @ResponseBody Long createAppointmentByPatient(@PathVariable Long patientId,
+                                                         @RequestBody @Valid Appointment appointment){
+        appointmentRepository.save(appointment);
+        return appointment.getId();
+    }
+
 }
