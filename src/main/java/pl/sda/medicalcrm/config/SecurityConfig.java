aaa1b40a23037ada0e0b4b27fa5ee968b25fe5c3
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-07-25 11:24:55 +0200

    changed SecurityConfig

--- src/main/java/pl/sda/medicalcrm/config/SecurityConfig.java
+++ src/main/java/pl/sda/medicalcrm/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package pl.sda.medicalcrm.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,30 +13,27 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    CustomUserDetailsService customUserDetailsService;
+    private final DataSource dataSource;
 
-    @Autowired
-    private JwtAuthenticationEntryPiont jwtAuthenticationEntryPiont;
+    SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-    @Bean
-    public JwtAthenticationFilter jwtAthenticationFilter(){
-        return new JwtAuthenticationFilter;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/signIn", "/register")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Bean
@@ -49,15 +41,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeRequests()
-                .antMatchers("/", "/home", "/signIn", "/register").permitAll()
-                .anyRequest().authenticated();
-
-        http.addFilterBefore(jwtAthenticationFilter(), UsenemePasswordAuthenticationFilter.class);
-    }
 }
