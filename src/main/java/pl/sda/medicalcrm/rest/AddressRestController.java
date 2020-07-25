630d7dd1568d127c750caac7455549cbc5847641
Grzegorz Albrewczynski <g.albrewczynski@gmail.com>
2020-07-25 10:03:28 +0200

    AddressRestController add findAddress

--- src/main/java/pl/sda/medicalcrm/rest/AddressRestController.java
+++ src/main/java/pl/sda/medicalcrm/rest/AddressRestController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.medicalcrm.entity.Address;
-import pl.sda.medicalcrm.entity.Specialization;
 import pl.sda.medicalcrm.repository.AddressRepository;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/addresses")
@@ -25,15 +25,35 @@ public class AddressRestController {
     @PostMapping
     public @ResponseBody
     Long createAddress(@RequestBody @Valid Address address) {
-        if (checkForAddress(address)) return 0L;
-        addressRepository.save(address);
+        if (checkForAddress(address)) {
+            return 0L;
+        } else {
+            addressRepository.save(address);
+        }
         return address.getId();
     }
 
-    protected boolean checkForAddress(Address address) {
-        return listAddresses().stream()
-                .anyMatch(s -> s.getId().equals(address.getId()));
+    @GetMapping(path = "/addresses/{addressId}")
+    public Address findAddress(Long addressId) {
+        Optional<Address> addressOptional = addressRepository.findById(addressId);
+        return addressOptional.get();
     }
+
+
+    private boolean checkForAddress(Address address) {
+        return listAddresses().stream()
+                .anyMatch(s -> s.getCity().equals(address.getCity())
+                        && s.getCountry().equals(address.getCountry())
+                        && s.getStreet().equals((address.getStreet()))
+                        && s.getZipCode().equals(address.getZipCode())
+                );
+    }
+//
+//    protected boolean checkForAddressById(Long addressId) {
+//        return listAddresses().stream()
+//                .anyMatch(s -> s.getId().equals(addressId)
+//                );
+//    }
 
 }
 
--- src/main/java/pl/sda/medicalcrm/rest/ClinicRestController.java
+++ src/main/java/pl/sda/medicalcrm/rest/ClinicRestController.java
@@ -1,44 +1,44 @@
-package pl.sda.medicalcrm.rest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import pl.sda.medicalcrm.entity.Address;
-import pl.sda.medicalcrm.entity.Clinic;
-import pl.sda.medicalcrm.repository.AddressRepository;
-import pl.sda.medicalcrm.repository.ClinicRepository;
-
-import javax.validation.Valid;
-
-
-
-@Controller
-@RequestMapping(path = "/clinics")
-public class ClinicRestController {
-
-    @Autowired
-    private ClinicRepository clinicRepository;
-
-    @Autowired
-    private AddressRepository addressRepository;
-
-    @Autowired
-    private AddressRestController addressRestController;
-
-    @PostMapping
-    public @ResponseBody
-    Long createClinic(@RequestBody @Valid Address address) {
-
-        addressRepository.save(address);
-
-        Clinic clinic = new Clinic();
-        clinic.setAddress(address);
-        clinicRepository.save(clinic);
-        return clinic.getId();
-    }
-}
+//package pl.sda.medicalcrm.rest;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import pl.sda.medicalcrm.entity.Address;
+//import pl.sda.medicalcrm.entity.Clinic;
+//import pl.sda.medicalcrm.repository.AddressRepository;
+//import pl.sda.medicalcrm.repository.ClinicRepository;
+//
+//import javax.validation.Valid;
+//
+//
+//
+//@Controller
+//@RequestMapping(path = "/clinics")
+//public class ClinicRestController {
+//
+//    @Autowired
+//    private ClinicRepository clinicRepository;
+//
+//    @Autowired
+//    private AddressRepository addressRepository;
+//
+//    @Autowired
+//    private AddressRestController addressRestController;
+//
+//    @PostMapping
+//    public @ResponseBody
+//    Long createClinic(@RequestBody @Valid Long addressId) {
+//
+//        Address address = addressRestController.findAddress(addressId);
+//
+//        Clinic clinic = new Clinic();
+//        clinic.setAddress(address);
+//        clinicRepository.save(clinic);
+//        return clinic.getId();
+//    }
+//}
 
 
