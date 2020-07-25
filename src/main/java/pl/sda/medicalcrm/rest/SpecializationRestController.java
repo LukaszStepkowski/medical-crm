2dba635f477142b59931c631e7a7122016ab32da
Malgorzata Tabor <tabor.malgosia@gmail.com>
2020-07-25 08:25:09 +0200

    new method

--- src/main/java/pl/sda/medicalcrm/rest/SpecializationRestController.java
+++ src/main/java/pl/sda/medicalcrm/rest/SpecializationRestController.java
@@ -12,6 +12,7 @@ import pl.sda.medicalcrm.repository.UserRepository;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping(path = "/specializations")
@@ -24,17 +25,17 @@ public class SpecializationRestController {
     private UserRepository userRepository;
 
     @GetMapping
-    public List<Specialization> listSpecializations(){
+    public @ResponseBody List<Specialization> listSpecializations(){
         return (List<Specialization>) specializationRepository.findAll();
     }
-
+/*
     @PostMapping
     public @ResponseBody Long createSpecialization(@RequestBody @Valid Specialization specialization){
         //TODO could need to set new ArrayList before saving to repository
         specializationRepository.save(specialization);
         return specialization.getId();
-    }
-
+    }*/
+/*
     @PutMapping(path = "/{doctorId}/{specializationId}")
     public @ResponseBody Long connectSpecializationDoctor(@PathVariable Long doctorId,
                                                           @PathVariable Long specializationId,
@@ -50,5 +51,42 @@ public class SpecializationRestController {
         specialization.getDoctors().add(doctor);
         specializationRepository.save(specialization);
         return specialization.getId();
+    }*/
+
+
+
+
+    @PostMapping
+    public @ResponseBody Long createSpecialization(@RequestBody @Valid Specialization specialization){
+        if (checkForSpecialization(specialization )){
+            specializationRepository.save(specialization);
+            return specialization.getId();
+        } else return 0L;
     }
+    @PutMapping(path = "/{doctorId}/{specializationId}")
+    public @ResponseBody Long connectSpecializationDoctor(@PathVariable Long doctorId,
+                                                          @PathVariable Long specializationId) {
+        Optional<Specialization> specializationOptional = specializationRepository.findById(specializationId);
+        if (!specializationOptional.isPresent()) return 0L;
+        Optional<User> doctorOptional = userRepository.findById(doctorId);
+        if (!doctorOptional.isPresent()) return 0L;
+        Specialization specialization = specializationOptional.get();
+        User doctor = doctorOptional.get();
+        specialization.setId(specializationId);
+        specialization.getDoctors().add(doctor);
+        specializationRepository.save(specialization);
+        return specialization.getId();
+    }
+    private boolean checkForSpecialization(Specialization specialization) {
+        if (listSpecializations().contains(specialization )) {
+
+
+            return true;
+        } else {
+            return false;
+
+        }
+
+    }
+
 }
