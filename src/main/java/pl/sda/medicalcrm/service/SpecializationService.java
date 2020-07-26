b969582ce6c0bb0e04baa46d901ceedef5a2289e
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-07-26 12:30:48 +0200

    refactored specialization rest controller and service

--- src/main/java/pl/sda/medicalcrm/service/SpecializationService.java
+++ src/main/java/pl/sda/medicalcrm/service/SpecializationService.java
@@ -1,57 +1,54 @@
-//package pl.sda.medicalcrm.service.specialization;
-//
-//import org.springframework.stereotype.Service;
-//import pl.sda.medicalcrm.dto.SpecializationDto;
-//import pl.sda.medicalcrm.entity.Doctor;
-//import pl.sda.medicalcrm.entity.Specialization;
-//import pl.sda.medicalcrm.entity.User;
-//import pl.sda.medicalcrm.repository.SpecializationRepository;
-//import pl.sda.medicalcrm.repository.UserRepository;
-//import pl.sda.medicalcrm.service.UserMapper;
-//
-//
-//import javax.transaction.Transactional;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Service
-//public class SpecializationService {
-//    private final SpecializationRepository specializationRepository;
-//    private final SpecializationMapper specializationMapper;
-//    private final UserRepository userRepository;
-//    private final UserMapper userMapper;
-//
-//
-//    public SpecializationService(SpecializationRepository repository, SpecializationMapper mapper, UserRepository userRepository, UserMapper userMapper) {
-//        this.specializationRepository = repository;
-//        this.specializationMapper = mapper;
-//        this.userRepository = userRepository;
-//        this.userMapper = userMapper;
-//    }
-//
-//    @Transactional
-//    public Specialization createSpecialization(String typeOfSpecialization) {
-//        var specialization = new Specialization(typeOfSpecialization);
-//        return specializationRepository.save(specialization);
-//    }
-//
-//    public List<SpecializationDto> listSpecialization() {
-//        return specializationMapper.mapSpecialization(specializationRepository.findAll());
-//    }
-//
-//
-//    @Transactional
-//    public void connectSpecializationDoctor(UUID userId, UUID specializationId) {
-//        specializationRepository.findById(specializationId).ifPresent(specialization -> userRepository.findById(userId)
-//                .ifPresent(user -> addDoctorToSpecialization(specialization,  user)));
-//    }
-//
-//    @Transactional
-//    private void addDoctorToSpecialization(Specialization specialization, User user) {
-//
-//
-//        specialization.addDoctor(user);
-//        specializationRepository.save(specialization);
-//    }
-//}
+package pl.sda.medicalcrm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.sda.medicalcrm.entity.Doctor;
+import pl.sda.medicalcrm.entity.Specialization;
+import pl.sda.medicalcrm.repository.SpecializationRepository;
+import pl.sda.medicalcrm.repository.UserRepository;
+
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class SpecializationService {
+
+    @Autowired
+    private SpecializationRepository specializationRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<Specialization> getAllSpecializationsList() {
+        return (List<Specialization>) specializationRepository.findAll();
+    }
+
+    @Transactional
+    public Long createNewSpecialization(Specialization specialization) {
+        if (!isSpecializationInDataBase(specialization)) return 0L;
+        specializationRepository.save(specialization);
+        return specialization.getId();
+    }
+
+    public String deleteSpecialization(Long specializationId){
+        specializationRepository.deleteById(specializationId);
+        return "Specialization Deleted";
+    }
+
+    @Transactional
+    public Long addDoctorToSpecialization(Long specializationId, Long doctorId) {
+        Specialization specialization = specializationRepository.findById(specializationId).get();
+        Doctor doctor = (Doctor) userRepository.findById(doctorId).get();
+        specialization.getDoctors().add(doctor);
+        specialization.setId(specializationId);
+        specializationRepository.save(specialization);
+        return specialization.getId();
+    }
+
+    private boolean isSpecializationInDataBase(Specialization specialization) {
+        List<Specialization> specializations = (List<Specialization>) specializationRepository.findAll();
+        return specializations.stream()
+                .anyMatch(s -> s.getTypeOfSpecialization().equals(specialization.getTypeOfSpecialization()));
+    }
+}
